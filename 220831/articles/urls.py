2f173b7d2fee6519217a4ad2804ade20b550919d
OctoHun <treeman@kakao.com>
2022-09-01 17:54:22 +0900

    220901

--- 220831/articles/urls.py
+++ 220831/articles/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
+    path('<int:pk>/', views.detail, name='detail'), # URL로 특정 게시글 조회하는 번호를 받음
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
+    path('<int:pk>/update/', views.update, name='update'),
 ]
--- 220831/articles/views.py
+++ 220831/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
@@ -16,23 +16,50 @@ def new(request):
 def create(request):
     # 사용자의 데이터를 받아서 DB에 저장
     # 요청에 대한 모든 데이터는 request
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
 
     # DB에 저장
     # 데이터 저장 방법 3가지
     # 1. 
-    article = Article()
-    article.title = title
-    article.content = content
-    article.save()
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
     
     # 2.
-    # article = Article(title=title, content=content)
-    # article.save()
+    article = Article(title=title, content=content)
+    article.save()
     
     # 3.
     # Article.objects.create(title=title, content=content)
     # 저장 전에 검증 시간이 필요하기에 3번보다는 2번이 좋음
 
-    return render(request, 'articles/create.html')
+    # return render(request, 'articles/index.html', context)
+    return redirect('articles:detail', article.pk) # '/articles/' 도 가능
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article':article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
