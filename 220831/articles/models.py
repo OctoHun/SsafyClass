84199ba53b830fb6d331fab10686ed2c0d90439e
OctoHun <97264011+OctoHun@users.noreply.github.com>
2022-09-06 08:48:08 +0900

    220906

--- 220831/articles/models.py
+++ 220831/articles/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=10)
-    content = models.TextField()
+    title = models.CharField(max_length=10) # 필수 키워드 인자 max_length 최대 255
+    content = models.TextField() # 글자 수 많을 때(최대길이 각각 다름)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
--- swea/test.py
+++ swea/test.py
@@ -1,3 +1,1 @@
-for i in range(5):
-    pass
-print(i)
+import math
