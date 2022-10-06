52fd6ddf9c22b0da9f4a1e61ba9075b84be750c9
OctoHun <97264011+OctoHun@users.noreply.github.com>
2022-10-06 22:22:23 +0900

    221006

---
+++ baekjoon/2775_B1.py
@@ -0,0 +1,12 @@
+t = int(input())
+arr = [[0]*15 for _ in range(15)]
+for i in range(15):
+    arr[0][i] = i
+for i in range(1, 15):
+    for j in range(1, 15):
+        for k in range(1, j+1):
+            arr[i][j] += arr[i-1][k]
+for tc in range(t):
+    k = int(input())
+    n = int(input())
+    print(arr[k][n])
