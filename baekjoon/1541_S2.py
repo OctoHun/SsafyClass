3ba9bcc93be033edc9c77104a87a800dae6b492b
OctoHun <97264011+OctoHun@users.noreply.github.com>
2022-10-27 21:40:47 +0900

    221027

--- baekjoon/1541_S2.py
+++ baekjoon/1541_S2.py
@@ -1,16 +1,30 @@
 import sys
 from collections import deque
 string = sys.stdin.readline().rstrip()
-arr = deque(string)
+arr1 = deque()
+arr2 = deque()
 length = len(string)
+number = ''
 for i in range(length):
-    if arr[i] == '+':
-        target1 = ''
-        target2 = ''
-        for j in range(1, 6):
-            if arr[i+j] != '+' and arr[i+j] != '-':
-                target1 += arr[i+j]
-            if arr[i - j] != '+' and arr[i + j] != '-':
-                target2 += arr[i-j]
-
+    if string[i] != '-' and string[i] != '+':
+        number += string[i]
+    else:
+        arr1.append(int(number))
+        arr1.append(string[i])
+        number = ''
+arr1.append(int(number))
+length = len(arr1)
+flag = 0
+for i in range(length):
+    if flag == 0:
+        if arr1[i] != '+' and arr1[i] != '-':
+            arr2.append(arr1[i])
+        elif arr1[i] == '+':
+            flag = 1
+    else:
+        flag = 0
+        temp = arr2.pop()
+        arr2.append(temp+arr1[i])
+first = arr2.popleft()
+print(first - sum(arr2))
 
