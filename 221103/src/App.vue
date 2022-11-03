2b0cd0facd7cfc3140292300cfec40ff2b1f8974
OctoHun <treeman@kakao.com>
2022-11-03 17:58:26 +0900

    221103

---
+++ 221103/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>캐릭터 진화 단계 가이드</h1>
+    <button class="header-button mx-5 mt-5">Home</button>
+    <button class="header-button">Start</button>
+    <homeVue v-if="renderHome"/>
+    <i class="fa-solid fa-circle-chevron-right"></i>
+    <i class="fa-solid fa-circle-chevron-left"></i>
+  </div>
+</template>
+
+<script>
+import homeVue from '@/components/home'
+
+export default {
+  name: 'App',
+  components: {
+    homeVue,
+  },
+  data: function () {
+    return {
+      page: 1,
+      isStarted: true,
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Noto Sans KR', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.header-button {
+  width: 100px;
+  height: 50px;
+  border: 1px solid rgb(255, 208, 0);
+  background-color: white;
+}
+.box {
+  width: 760px;
+  height: 405px;
+}
+</style>
---
+++ 221103/src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 class="my-5">Start 버튼으로 시작해 보세요!</h2>
+    <div class="box mx-auto">
+        <img src="../assets/ssafy-banner.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'homeVue',
+}
+</script>
+
+<style>
+
+</style>
